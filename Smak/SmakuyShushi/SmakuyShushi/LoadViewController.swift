505131246f2384e7950e259bd202f197d59aea6d
mikhail <lokal1122@mail.ru>
2022-03-20 21:45:01 +0300

    NewFile

---
+++ Smak/SmakuyShushi/SmakuyShushi/LoadViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoadViewController.swift
+//  SmakuyShushi
+//
+//  Created by Mikhail Kim on 03.03.2022.
+//
+
+import Foundation
+import UIKit
+
+
+ class LoadViewController: UIViewController{
+    
+
+    
+    @IBOutlet weak var smakShu: UIImageView!
+    
+    
+     
+    override func viewDidLoad() {
+     super.viewDidLoad()
+              // Do any additional setup after loading the view.
+        
+//        smakShu.image = UIImage(named: "1.1")
+//
+//               smakShu.animationImages = [UIImage(named: "1.2"),UIImage(named: "1.3"),UIImage(named: "1.1")] as! [UIImage]
+//
+//               smakShu.animationDuration = 1.5
+//               smakShu.animationRepeatCount = 3
+//
+//               smakShu.startAnimating()
+
+    }
+ 
+    
+
+    }
+
+
+    
+
